3f984543f900eb8effd29a69692b40cf9409a495
Mustafaelhdadi <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-01-03 23:05:34 +0200

    Updates

---
+++ precache-manifest.36b91a2bd92afa7d72108ed347bf45d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9448f506afcc9f0f65dad49cada8b6",
+    "url": "/monsters-react-search-app/index.html"
+  },
+  {
+    "revision": "86f7d5ffb03f6e854436",
+    "url": "/monsters-react-search-app/static/css/main.ff286055.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-react-search-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-search-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f7d5ffb03f6e854436",
+    "url": "/monsters-react-search-app/static/js/main.05eb1a77.chunk.js"
+  },
+  {
+    "revision": "c7b1947fe7e33c519906",
+    "url": "/monsters-react-search-app/static/js/runtime-main.e07fe56a.js"
+  }
+]);
